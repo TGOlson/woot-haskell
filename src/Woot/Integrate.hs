cccdaaaa7e4c7e0566e3c3414fe7358c434e1a5f
Tyler Olson <tydotg@gmail.com>
2015-08-29 10:25:50 -0700

    Maybe insert

--- src/Woot/Integrate.hs
+++ src/Woot/Integrate.hs
@@ -8,7 +8,7 @@ import Woot.Operation
 
 -- return the new WString on success, on failure returns an integration error
 integrate :: Operation -> WString -> Either String WString
-integrate op ws = if canIntegrate op ws then undefined else integrationError
+integrate op ws = if canIntegrate op ws then Right $ integrateOp op ws else integrationError
   where
     integrationError = Left $ "Cannot integrate. Operation should be added to a queue and retried."
 
@@ -18,11 +18,11 @@ integrateOp (Operation Delete _ _) ws = undefined
 
 
 integrateInsert :: WChar -> WString -> WString
-integrateInsert wc ws = if isEmpty sub then maybeInsert (indexOf (wCharNextId wc) ws) wc ws else undefined
+integrateInsert wc ws = if isEmpty sub then maybeInsert (indexOf (wCharNextId wc) ws) else undefined
   where
     sub = subsection (wCharPrevId wc) (wCharNextId wc) ws
     -- should always be safe to get index and insert since we have flagged this as 'canIntegrate'
-    maybeInsert mi wc ws = maybe ws (\i -> insert i wc ws) mi
+    maybeInsert mi = maybe ws (\i -> insert i wc ws) mi
 
 canIntegrate :: Operation -> WString -> Bool
 canIntegrate (Operation Insert sid wc) ws = undefined
